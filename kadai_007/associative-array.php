9a8dacf9a758dffa0ab63b57d69401bfd82e93ba
satokonopon <m.hishiya@gmail.com>
2025-02-20 22:16:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP課題</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $print_r = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+       print_r($print_r);
+       ?>
+   </p>
+</body>
+</html>
