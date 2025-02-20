7f44b28d61e41b47393d938354bac7fde0a5549c
satokonopon <m.hishiya@gmail.com>
2025-02-20 21:44:29 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP課題</title>
+</head>
+
+<body>
+<p>
+<?php
+$numbers = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+
+$sum = array_sum($numbers);
+$count = count($numbers);
+$average = $sum / $count;
+echo "平均値: " . $average;
+?>
+</p>
+</body>
+</html>
