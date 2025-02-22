4649af0f5bbe5bf81f73e9561a08fe9e5bde15ae
satokonopon <m.hishiya@gmail.com>
2025-02-23 00:44:21 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+<body>
+<?php
+// 連想配列を作成
+$vegetable = array(
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'
+);
+
+// foreach文とecho文を使ってキーと値を出力
+foreach ($vegetable as $key => $value) {
+    echo $key . "：" . $value . "<br>"; // コロン（：）で区切り、改行する
+}
+?>
+</body>
+</html>
